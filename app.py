d36746568a764e357cf66a9f5235f55116bd7f55
Alexander <17806067729@163.com>
2024-10-14 21:30:57 +0800

    'a'

--- app.py
+++ app.py
@@ -4,11 +4,18 @@ from utils import data_processing, file_handling
 from config import settings
 import os
 
+def read_markdown_file(markdown_file):
+    with open(markdown_file, "r", encoding="utf-8") as file:
+        return file.read()
 
 st.set_page_config(page_title="ModernY短视频平台", layout="wide")
 
 st.title("ModernY短视频智能平台")   
 
+# 读取并显示 introduction.md 文件内容
+introduction_path = os.path.join("content_scripts", "introduction.md")
+introduction_content = read_markdown_file(introduction_path)
+st.markdown(introduction_content, unsafe_allow_html=True)
 
 # 页脚
 st.markdown("---")
