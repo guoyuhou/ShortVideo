47782455ed4b157377c106d0166cad8fe735655c
Alexander <17806067729@163.com>
2024-10-14 22:09:11 +0800

    'a'

--- pages/05_工作日志.py
+++ pages/05_工作日志.py
@@ -1,1 +1,12 @@
-import streamlit
+import streamlit as st
+import os
+
+def read_markdown_file(markdown_file):
+    with open(markdown_file, "r", encoding="utf-8") as file:
+        return file.read()
+
+st.title("工作日志")
+
+introduction_path = os.path.join("content_scripts", "introduction.md")
+introduction_content = read_markdown_file(introduction_path)
+st.markdown(introduction_content, unsafe_allow_html=True)
